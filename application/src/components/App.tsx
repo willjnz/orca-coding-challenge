e5e9a64c4e953f21d1068a9730e2e57893dc4a36
willjnz <will.jones@hotmail.co.nz>
2024-12-18 16:54:43 +0100

    style layer. add under labels. fix initial map zoom

--- application/src/components/App.tsx
+++ application/src/components/App.tsx
@@ -3,7 +3,6 @@ import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 export default function App(): JSX.Element {
-
   const mapContainer = useRef(null); // Create a reference for the map container
 
   type TInterval = '5' | '10' | '50';
@@ -16,7 +15,7 @@ export default function App(): JSX.Element {
       accessToken: import.meta.env.VITE_MAPBOX_ACCESS_TOKEN,
       container: mapContainer.current as unknown as HTMLElement,
       style: 'mapbox://styles/mapbox/streets-v12',
-      center: [-120.759538, 35.130000],
+      center: [-120.759538, 35.13],
       zoom: 8,
     });
 
@@ -34,53 +33,33 @@ export default function App(): JSX.Element {
           `http://localhost:8081/collections/public.bathymetry_contours_${selectedInterval}/tiles/{z}/{x}/{y}?properties=id,depth_m&limit=10000`,
         ],
       });
-      map.addLayer({
-        id: 'contours',
-        source: 'contours',
-        'source-layer': 'default',
-        type: 'line',
-        paint: {
-          'line-color': '#ff0000', // Red color for the contours
-          'line-width': 2, // Line width
+      map.addLayer(
+        {
+          id: 'contours',
+          source: 'contours',
+          'source-layer': 'default',
+          type: 'line',
+          paint: {
+            'line-width': 2,
+            'line-color': [
+              'interpolate',
+              ['linear'],
+              ['to-number', ['get', 'depth_m']],
+              -7000,
+              '#4B0082',
+              -4000,
+              '#191970',
+              -1000,
+              '#00008B',
+              -200,
+              '#4682B4',
+              -0,
+              '#A0D3FF',
+            ],
+          },
         },
-        // paint: {
-        //   'circle-radius': 4,
-        //   'circle-color': [
-        //     'interpolate',
-        //     ['linear'],
-        //     ['to-number', ['get', mapVariable]],
-        //     -1.3922,
-        //     '#FFEDA0',
-        //     82.717625,
-        //     '#FEB24C',
-        //     165.43525,
-        //     '#F03B20',
-        //     // TODO: get the data range from a new azure function so that we can symobolize a layer nicely
-        //     // // TODO: style this based on the mapVariable
-        //     // 0,
-        //     // 'rgba(255, 1, 1, 1)', // Extrem niedrig (< 5. Perzentil)
-        //     // 5,
-        //     // 'rgba(255, 1, 1, 1)', // Extrem niedrig (< 5. Perzentil)
-        //     // 15,
-        //     // 'rgba(254, 71, 72, 1)', // Sehr niedrig (> 5. - 15. Perzentil)
-        //     // 25,
-        //     // 'rgba(254, 71, 72, 1)', // Niedrig (> 15. - 25. Perzentil)
-        //     // 50,
-        //     // 'rgba(227, 165, 192, 1)', // Normal (> 25. - 75. Perzentil)
-        //     // 75,
-        //     // 'rgba(138, 130, 247, 1)', // Hoch (> 75. - 85. Perzentil)
-        //     // 85,
-        //     // 'rgba(72, 71, 254, 1)', // Sehr hoch (> 85. - 95. Perzentil)
-        //     // 95,
-        //     // 'rgba(72, 71, 254, 1)', // Sehr hoch (> 85. - 95. Perzentil)
-        //     // 100,
-        //     // 'rgba(0, 2, 255, 1)', // Extrem hoch (> 95. Perzentil)
-        //     // 'rgba(128, 128, 128, 1)', // Default color if no match (optional)
-        //   ],
-        //   'circle-stroke-color': '#fff',
-        //   'circle-stroke-width': 1,
-        // },
-      });
+        'building-number-label'
+      ); // add this layer under the map labels
     });
 
     // Cleanup function to remove the map when the component unmounts
